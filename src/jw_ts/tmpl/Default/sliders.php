d61b314f895d4aee469743fef37d8f5807d10c57
Anderson Grudtner Martins <anderson@ostraining.com>
2016-10-13 19:36:49 -0300

    Rebrand for Joomlashack

--- src/jw_ts/tmpl/Default/sliders.php
+++ src/jw_ts/tmpl/Default/sliders.php
@@ -1,9 +1,9 @@
 <?php
 /**
  * @package    Tabs and Sliders [for articles]
- * @contact    www.alledia.com, hello@alledia.com
+ * @contact    www.joomlashack.com, hello@joomlashack.com
  * @author     JoomlaWorks - http://www.joomlaworks.net
- * @author     Alledia - http://www.alledia.com
+ * @author     Alledia - http://www.joomlashack.com
  * @copyright  Copyright (c) 2006 - 2015 JoomlaWorks Ltd. All rights reserved.
  * @copyright  Copyright (c) 2016 Open Source Training, LLC. All rights reserved
  * @license    GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
