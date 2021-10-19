db33845a3d16df35457a1390944e0a757af8ee73
Bill Tomczak <bill@joomlashack.com>
2021-10-19 09:08:52 -0700

    Update file headers

--- src/jw_ts/includes/js/behaviour.js
+++ src/jw_ts/includes/js/behaviour.js
@@ -1,25 +1,24 @@
 /**
- * @package   Tabs and Sliders
+ * @package   TabsSliders
  * @contact   www.joomlashack.com, help@joomlashack.com
- * @author    JoomlaWorks - http://www.joomlaworks.net
  * @copyright 2006-2015 JoomlaWorks Ltd. All rights reserved.
- * @copyright 2016-2019 Joomlashack.com. All rights reserved
- * @license   http://www.gnu.org/licenses/gpl.html GNU/GPL
+ * @copyright 2016-2021 Joomlashack.com. All rights reserved
+ * @license   https://www.gnu.org/licenses/gpl.html GNU/GPL
  *
- * This file is part of Tabs and Sliders.
+ * This file is part of TabsSliders.
  *
- * Tabs and Sliders is free software: you can redistribute it and/or modify
+ * TabsSliders is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2 of the License, or
  * (at your option) any later version.
  *
- * Tabs and Sliders is distributed in the hope that it will be useful,
+ * TabsSliders is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Tabs and Sliders.  If not, see <http://www.gnu.org/licenses/>.
+ * along with TabsSliders.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 /* Copyright (c) Patrick Fitzgerald - Version 1.9 */
